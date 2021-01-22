d2d90f8a0ccd54130560f11b8b87188b2c16cda9
unknown <victor_marcantonio@hotmail.com>
2021-01-22 19:52:58 -0300

    urna finalizada

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '36111',
+                nome: 'Paula Oliveira',
+                partido: 'PAC',
+                fotos:[
+                    {url:'cv2.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '25941',
+                nome: 'Rogerio Correia',
+                partido: 'PMN',
+                fotos:[
+                    {url:'cv1.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '36',
+                nome:'Marcio Bolinha',
+                partido: 'PAC',
+                vice:"Oseas",
+                fotos:[
+                    {url:'v3.jpg', legenda: 'Prefeito', small: false},
+                    {url:'v4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '25',
+                nome:'Aparecida Leme',
+                partido: 'PMN',
+                vice:"Cicero",
+                fotos:[
+                    {url:'v1.jpg', legenda: 'Prefeito', small: false},
+                    {url:'v2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+    
+        ]
+    }
+];
